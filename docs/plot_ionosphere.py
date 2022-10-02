a7112c89ae8ed7d01454159a29600bd61974a51f
yukama18 <yukama.tokyo@gmail.com>
2022-10-02 16:03:02 -0700

    four day plot

--- docs/plot_ionosphere.py
+++ docs/plot_ionosphere.py
@@ -6,7 +6,7 @@ import datetime
 import plotly.express as px
 
 # read and extract longitude, latitude, altitude, and e_density from ISS data
-df = pd.read_csv("ISS_SP_FPMU_20220912_20220913_20220914_half.csv")
+df = pd.read_csv("ISS_SP_FPMU_20200911_20200912_20200913_20200914_half.csv")
 # df = df[df["SUNLIGHT_%"] != 0]
 latitude = df.iloc[:,1].values   #float64
 longitude = df.iloc[:,2].values
@@ -14,15 +14,12 @@ altitude = df.iloc[:,3].values
 e_density = df.iloc[:,4].values
 
 # making the plot
-
-plot_data=[ionosphere]
-
 fig = go.Figure(go.Scattergeo(
         lon = longitude,
         lat = latitude,
         mode = 'markers',
         marker = dict(
-            size = 10,
+            size = 15,
             opacity = 0.08,
             reversescale = True,
             autocolorscale = False,
